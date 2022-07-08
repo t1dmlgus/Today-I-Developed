4f779261a1cb81a5ed2f44bfe7beca7cb4ef3783
t1dmlgus <dmlgusgngl@gmail.com>
2022-07-08 18:23:19 +0900

    feat: init order-mvp

---
+++ order-mvp/src/test/java/com/t1dmlgus/ordermvp/OrderMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.ordermvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
