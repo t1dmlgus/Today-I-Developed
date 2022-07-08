4974b1845a1378994b941564080a9d82496ae9e7
t1dmlgus <59961350+t1dmlgus@users.noreply.github.com>
2022-07-08 18:25:35 +0900

    feat: init order-mvp (#1)

---
+++ order-mvp/src/main/java/com/t1dmlgus/ordermvp/OrderMvpApplication.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.ordermvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMvpApplication.class, args);
+	}
+
+}
