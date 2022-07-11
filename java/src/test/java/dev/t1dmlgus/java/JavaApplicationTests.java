1246bfa4d2dce376d178de390502f3f282e7c1aa
t1dmlgus <59961350+t1dmlgus@users.noreply.github.com>
2022-07-11 19:18:13 +0900

    init java (#6)

---
+++ java/src/test/java/dev/t1dmlgus/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.t1dmlgus.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
