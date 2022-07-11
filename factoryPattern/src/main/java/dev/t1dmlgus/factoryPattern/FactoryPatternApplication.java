e4439e38eec5440c071bfd09b3e8659185d7aca6
t1dmlgus <59961350+t1dmlgus@users.noreply.github.com>
2022-07-11 17:02:21 +0900

    init factoryPattern (#5)

---
+++ factoryPattern/src/main/java/dev/t1dmlgus/factoryPattern/FactoryPatternApplication.java
@@ -0,0 +1,13 @@
+package dev.t1dmlgus.factoryPattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryPatternApplication.class, args);
+	}
+
+}
---
+++ factoryPattern/src/test/java/dev/t1dmlgus/factoryPattern/FactoryPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.t1dmlgus.factoryPattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
