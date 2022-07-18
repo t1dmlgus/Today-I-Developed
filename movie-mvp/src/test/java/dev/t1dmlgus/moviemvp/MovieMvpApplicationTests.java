d55e97febfeab2dffa25421d7be479388188a987
t1dmlgus <dmlgusgngl@gmail.com>
2022-07-18 11:27:25 +0900

    init movie-mvp

---
+++ movie-mvp/src/test/java/dev/t1dmlgus/moviemvp/MovieMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.t1dmlgus.moviemvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
