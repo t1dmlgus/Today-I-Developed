26ef26ea29a5917302596c105e7285200b5d3c40
t1dmlgus <59961350+t1dmlgus@users.noreply.github.com>
2022-07-18 23:40:09 +0900

    init movie-mvp (#15)
    * init movie-mvp
    
    * docs: 영화 예매 시스템 readme.md(오브젝트)

---
+++ movie-mvp/src/main/java/dev/t1dmlgus/moviemvp/MovieMvpApplication.java
@@ -0,0 +1,13 @@
+package dev.t1dmlgus.moviemvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMvpApplication.class, args);
+	}
+
+}
