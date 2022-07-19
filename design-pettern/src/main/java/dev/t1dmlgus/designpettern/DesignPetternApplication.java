08c92e38655d093a89d35ed7f4a16a5338d03761
t1dmlgus <59961350+t1dmlgus@users.noreply.github.com>
2022-07-19 23:30:14 +0900

    팩토리 패턴 & 추상 팩토리 패턴 (#16)
    * init design-pattern
    
    * docs: 팩토리 패턴 작성
    
    * feat: 팩토리 패턴, 추상 팩토리 패턴 구현

---
+++ design-pettern/src/main/java/dev/t1dmlgus/designpettern/DesignPetternApplication.java
@@ -0,0 +1,13 @@
+package dev.t1dmlgus.designpettern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPetternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPetternApplication.class, args);
+	}
+
+}
---
+++ design-pettern/src/test/java/dev/t1dmlgus/designpettern/DesignPetternApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.t1dmlgus.designpettern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPetternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
