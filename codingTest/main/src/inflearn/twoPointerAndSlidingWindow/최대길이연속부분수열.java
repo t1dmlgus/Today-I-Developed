e911ecabf17953fe4a0f90a200f96f2492159a0e
t1dmlgus <dmlgusgngl@gmail.com>
2022-08-19 23:44:23 +0900

    twopoint: 최대길이 연속부분수열

---
+++ codingTest/main/src/inflearn/twoPointerAndSlidingWindow/최대길이연속부분수열.java
@@ -0,0 +1,41 @@
+package inflearn.twoPointerAndSlidingWindow;
+
+import java.util.Scanner;
+
+// twoPoint
+public class 최대길이연속부분수열 {
+
+    public static int solution(int n, int k, int[] arr) {
+
+        int answer = 0;
+        int cnt = 0;
+        int lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) {
+                cnt++;
+            }
+            while (cnt > k) {
+                if (arr[lt] == 0) {
+                    cnt--;
+                }
+                lt++;
+            }
+            answer = Math.max(answer, rt - lt + 1);
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(최대길이연속부분수열.solution(n, k, arr));
+    }
+}
